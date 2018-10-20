33f350aa7368577c207f5ea64730afb459b6ed75
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-10-20 14:03:43 -0300

    Setting project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    entry: "./src/App.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js"
+    },
+    devServer: {
+        inline: true,
+        contentBase: "./public",
+        port: "5000"
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}

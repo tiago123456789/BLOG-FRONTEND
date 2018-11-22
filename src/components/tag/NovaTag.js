4aa8438d032af8de91a7d5bdd8a57580fd5032cf
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-11-21 23:32:42 -0200

    Alteration on button cancel in page register tag

--- src/components/tag/NovaTag.js
+++ src/components/tag/NovaTag.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Panel from "../template/Panel";
 import Button from "../template/Button";
 
@@ -11,7 +12,9 @@ export default class NovaTag extends Component {
                     <i className="fa fa-close"></i> Salvar
                 </Button>&nbsp;
                 <Button size="sm" color="danger">
-                    <i className="fa fa-close"></i> Cancelar
+                    <Link to={"/tag"}><i className="fa fa-close text"></i>&nbsp;
+                        <span className="text-black">Cancelar</span>
+                    </Link>
                 </Button>
                 <Panel type="primary" title="Nova tag">
 
